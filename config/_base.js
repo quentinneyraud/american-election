492916c637cc9b8f994c3e03ced6caab7efe3fd1
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-09-28 12:02:02 +0200

    set localhost as url

--- config/_base.js
+++ config/_base.js
@@ -20,7 +20,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : localIp(),
+  server_host : 'localhost',
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
