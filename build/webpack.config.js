cff571173d0e623773c00d9305579c9405291d71
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-10-02 23:27:22 +0200

    add resources

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -259,7 +259,7 @@ webpackConfig.module.loaders.push(
   },
   {test: /\.eot(\?.*)?$/, loader: 'file?prefix=fonts/&name=[path][name].[ext]'},
   {test: /\.svg(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml'},
-  {test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
+  {test: /\.(png|jpg)$/, loader: 'url?limit=30000'}
 )
 /* eslint-enable */
 
